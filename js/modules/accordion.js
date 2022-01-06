d4cff09445b9557635f56779b4eaaffb1e6f397f
igordonatti <igordonatti.id@gmail.com>
2022-01-06 12:14:46 -0400

    refatorando accordion em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,20 +1,29 @@
-export default function initAccordion() {
-  const accordioniList = document.querySelectorAll(
-    '[data-anime="accordion"] dt'
-  );
-  const activeClass = "ativo";
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
   }
 
-  if (accordioniList.length) {
-    accordioniList[0].classList.add(activeClass);
-    accordioniList[0].nextElementSibling.classList.add(activeClass);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
 
-    accordioniList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+  // adiciona os eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
+  }
+
+  // iniciar funçao
+  init() {
+    if (this.accordionList.length) {
+      // ativar primeiro item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+
+    return this;
   }
 }
